dac9bf5405be0e1c9dc954405c0a7d777129e929
julie17-d <derbaljulie@gmail.com>
2022-10-04 16:42:48 +0200

    added sql table for login

--- reso_social.sql
+++ reso_social.sql
@@ -3,13 +3,24 @@ CREATE DATABASE IF NOT EXISTS `reso_social` DEFAULT CHARACTER SET utf8mb4 COLLAT
 USE `reso_social`;
 
 
-
-
 DROP TABLE IF EXISTS `critique`;
 
 CREATE TABLE `critique`(
-    `id`int(10) UNSIGNED NOT NULL,
+    `id`int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     `author` varchar(50) NOT NULL,
     `content` text NOT NULL,
-    `rating` int(1) UNSIGNED NOT NULL
-)
+    `rating` int(1) UNSIGNED NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login`(
+    `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `login` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
