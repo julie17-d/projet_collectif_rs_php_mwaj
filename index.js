926bf229bec9966d8fb69ea330bc3af325b8520a
julie17-d <derbaljulie@gmail.com>
2022-10-04 10:09:10 +0200

    feat: api connexion

---
+++ index.js
@@ -0,0 +1,12 @@
+const apiKey = "c65527ff";
+
+let search;
+
+async function fetchMovies() {
+    let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + "get out";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+fetchMovies();
