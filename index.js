c1336e7bae25aefead97ec775abd027229442e5f
loula <wmhadjou@gmail.com>
2022-10-06 15:38:35 +0200

    mi code

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const apiKey = "c65527ff";
 let search;
 
 async function fetchMovies() {
-    let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + "get out";
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+  let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + "get out";
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 fetchMovies();
