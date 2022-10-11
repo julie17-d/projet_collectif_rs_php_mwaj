070f7acb7f9a8e1a62963bd6d25ad98c27fd2b92
loula <wmhadjou@gmail.com>
2022-10-11 15:13:33 +0200

    test

--- login.php
+++ login.php
@@ -1,6 +1,80 @@
 <?php
 session_start();
+$name = "";
+$email = "";
+$errors=array();
+
+//connection a la database
+
+$db = mysqli_connect('localhost','root','root','reso_social');
+if(isset($_POST["submit"])){
+    var_dump ($_POST);
+
+//enregistrement de l'utilisateur 
+
+if(isset($_POST ['current_user'])){
+    //reception des valeurs entrées par l'utilisateur
+    $name = mysqli_real_escape_string($db,$_POST['name']);
+    $email = mysqli_real_escape_string($db,$_POST['email']);
+    $password = mysqli_real_escape_string($db,$_POST['password']);
+
+    
+
+}
+//validation et verification des input
+if (empty($name)) { array_push($errors, "nom requis"); 
+}
+if (empty($email)) { array_push($errors, "Email requis"); 
+}
+if (empty($password)) { array_push($errors, "mot de passe requis"); 
+}
+
+//verification si l'utlisateur existe deja ou pas 
+$user_check_query = "SELECT * FROM users_table WHERE name = '$name' OR email = '$email'";
+$result = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($result); 
+
+//si l'utilisateur existe 
+if ($user) { 
+    if ($user['name'] === $name) {
+      array_push($errors, "nom existe deja ");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email existe deja ");
+    }
+  }
+  //LOGIN UTILISATEUR
+  if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['name']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+  
+    if (empty($name)) {
+        array_push($errors, "nom requis");
+        echo "NON";
+    }
+    if (empty($password)) {
+        array_push($errors, "Mot de passe requis");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users_table WHERE name='$name' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['name'] = $name;
+          $_SESSION['succes'] = "vous etes connectés";
+          echo "ok";
+          
+        }else {
+            array_push($errors, "mauvaise combinaison du nom & mot de passe ");
+        }
+    }
+  }
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -27,31 +101,9 @@ session_start();
             <div class="login-box">
                 <h2>Login</h2>
 
-                <?php
-                $traitement=isset($_POST['name']);
-                var_dump($_POST);
-                if ($traitement)
-                {
-                    $verifieName = $_POST[$verifieName];
-                    $mysqli = new mysqli("localhost", "root", "root","reso_social");
-                    $verifieName = $mysqli->real_escape_string($verifieName);
+                
 
-                    $lInstructionSql = "SELECT * FROM ";
-
-                        $res = $mysqli->query($lInstructionSql);
-                        $user = $res->fetch_assoc();
-                        if ( ! $user OR $user["name"] != $verifieName)
-                        {
-                            echo "La connexion a échouée. ";
-
-                        }else{
-
-                            echo "Votre connexion est un succès : " . $user['alias'] . ".";
-
-                    $_SESSION['connected_id']=$user['id'];
-            }
-        }
-                ?>
+                       
 
                 <form action="login.php" method="POST">
 
@@ -67,9 +119,8 @@ session_start();
         
                     <div class="button-form">
         
-                        <a id="submit" href="#">
-                            Submit
-                        </a>
+                    <button type="submit" name="submit">SUBMIT</button>
+                           
         
                         <div id="register">
                             Don't have an account ?
@@ -92,31 +143,3 @@ session_start();
 </body>
 
 </html>
-<?php
-if($_SERVER ["REQUEST_METHOD"]== POST){
-    $host = "localhost";
-    $username = "root";
-    $passeword ="root";
-    $database="reso_social";
-
-    $name = $_POST ["name"];
-
-    if(!isset($name)){
-        die("rentrez votre nom");
-    }
-    $mysqli = new mysqli($host, $username, $password, $database);
-    if ($mysqli->connect_error) {
-        die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-      }  
-
-      $statement = $mysqli->prepare("INSERT INTO users_table(name) VALUES($name)");
-      $statement->bind_param('ss', $name); 
-    
-    if($statement->execute()){
-      print "Salut " . $name . "!, votre adresse e-mail est ";
-    }else{
-      print $mysqli->error; 
-    }
-  } 
-
-?>
